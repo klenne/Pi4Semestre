641fc4d5e1df7f6c47e428adbd47c0badc5f2f64
Guilherme Klesse de Miranda <guiklesse@gmail.com>
2018-10-23 20:32:45 -0200

    novos ajustes classe transacoes

--- src/model/bean/Transacoes.java
+++ src/model/bean/Transacoes.java
@@ -3,12 +3,18 @@ package model.bean;
 
 public class Transacoes {
 	private int codTransacao,codAnuncio,codUsuario;
-	private String data;
+	private String data, status;
 	public int getCodTransacao() {
 		return codTransacao;
 	}
 	public void setCodTransacao(int codTransacao) {
 		this.codTransacao = codTransacao;
+	}
+	public String getStatus() {
+		return status;
+	}
+	public void setStatus(String status) {
+		this.status = status;
 	}
 	public int getCodAnuncio() {
 		return codAnuncio;
