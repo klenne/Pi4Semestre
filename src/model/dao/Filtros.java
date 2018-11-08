3c4c425d29d8945e1917e3aa6209e1c294c9976f
Guilherme Klesse de Miranda <guiklesse@gmail.com>
2018-11-07 22:32:52 -0200

    corrigindo bug busca filtro venda

--- src/model/dao/Filtros.java
+++ src/model/dao/Filtros.java
@@ -5,62 +5,70 @@
  */
 package model.dao;
 
-
 /**
  *
  * @author USER
  */
 public class Filtros {// classe para adicionar filtros para as buscas de anuncios
-    
+
     private String filtrosSql[];// instrução sql
     private String filtrosMostrar[]; // para mostrar para o usuario o filtro 
-    
+
     public Filtros() {
         filtrosMostrar = new String[10];
-       filtrosSql = new String [10];
-       limpa();
+        filtrosSql = new String[10];
+        limpa();
     }
-    
-    public String [] getFiltrosSql() {
+
+    public String[] getFiltrosSql() {
         return filtrosSql;
     }
-    
+
     public String[] getFiltrosMostrar() {
         return filtrosMostrar;
     }
-    
+
     public String joinRequisito() {
         return " join  REQUISITO as r  on   ap.cod_anuncio=r.cod_anuncio ";
     }
-    public void filtroTitulo(String titulo){
-         filtrosMostrar[0]="Título: " + titulo;
-        filtrosSql[0]="and ap.titulo = '" + titulo.toUpperCase() + "' ";
-        
+
+    public void filtroTitulo(String titulo) {
+        filtrosMostrar[0] = "Título: " + titulo;
+        filtrosSql[0] = "and ap.titulo = '" + titulo.toUpperCase() + "' ";
+
     }
+
     public void filtroConsole(String console) {
-        filtrosMostrar[1]="No console: " + console.toUpperCase();
-        filtrosSql[1]="and ap.console = '" + console.toUpperCase() + "' ";
-        
+        filtrosMostrar[1] = "No console: " + console.toUpperCase();
+        filtrosSql[1] = "and ap.console = '" + console.toUpperCase() + "' ";
+
     }
 
     public void filtroRequisito(String requisito) {
-        filtrosMostrar[2]="Aceitando o jogo: " + requisito;
-        filtrosSql[2]="and r.interesse_em like '%" + requisito.toUpperCase() + "%' ";
-        
+        filtrosMostrar[2] = "Aceitando o jogo: " + requisito;
+        filtrosSql[2] = "and r.interesse_em like '%" + requisito.toUpperCase() + "%' ";
+
     }
-    public void filtroCategoria(String categoria){
-       filtrosMostrar[3]="Categoria: " + categoria;
-        filtrosSql[3]="and ap.tipo = '" + categoria + "' ";
+
+    public void filtroCategoria(String categoria) {
+        filtrosMostrar[3] = "Categoria: " + categoria;
+        filtrosSql[3] = "and ap.tipo = '" + categoria + "' ";
     }
-        public void filtroValor(String valor){
-       filtrosMostrar[4]="Tipo transacão: " + valor;
-        filtrosSql[4]="and ap.valor = '" + valor + "' ";
+
+    public void filtroValor(String valor) {
+        if (valor.equals("VENDA")) {
+
+            filtrosSql[4] = "and not ap.valor = 'TROCA' and not ap.valor='DOACAO' ";
+        } else {
+            filtrosSql[4] = "and ap.valor = '" + valor + "' ";
+        }
+        filtrosMostrar[4] = "Tipo transacão: " + valor;
+
     }
-    
 
     public void removerFiltro(int index) {
-        filtrosMostrar[index]="";
-        filtrosSql[index]="";
+        filtrosMostrar[index] = "";
+        filtrosSql[index] = "";
     }
 
     public String filtrosParaString() {
@@ -70,12 +78,13 @@ public class Filtros {// classe para adicionar filtros para as buscas de anuncio
         }
         return filtros;
     }
-    public void limpa(){
-     
-  for (int i = 0; i < filtrosSql.length; i++) {
-             filtrosSql[i]="";
-             filtrosMostrar[i]="";
+
+    public void limpa() {
+
+        for (int i = 0; i < filtrosSql.length; i++) {
+            filtrosSql[i] = "";
+            filtrosMostrar[i] = "";
         }
     }
-    
+
 }
